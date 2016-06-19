2239ab35e6d0a6e14373fbdcb0bbd860711afc3a
paulawojtysiak <pola.wojtysiak@gmail.com>
2016-06-19 15:36:25 +0200

    My Django Girls app, firt commit

---
+++ python_intro.py
@@ -0,0 +1,9 @@
+def hej(imie):
+    print('Hej '+imie+'!')
+dziewczyny = ['Rachel', 'Kasia', 'Ola', 'Ty']
+for imie in dziewczyny:
+    hej(imie)
+    print('Kolejna dziewczyna')
+
+for i in rage(2,27):
+    print(i)
